0a7cf9a01c8730ded72d45559737e3077bc612ca
Edun Joshua Olubori <joshuaolubori@gmail.com>
2024-07-22 09:18:24 +0000

    fixed omitted comma

--- include/logic/case_when_logic.py
+++ include/logic/case_when_logic.py
@@ -1,5 +1,5 @@
 def apply_case_when_logic(df):
-    columns_to_process = ['teams_did_not_score',"both_teams_scored","won_3_games_or_more", "draw"
+    columns_to_process = ['teams_did_not_score',"both_teams_scored","won_3_games_or_more", "draw",
         'over_0_5', 'over_1_5', 'over_2_5', 'over_3_5',
         'under_0_5', 'under_1_5', 'under_2_5', 'under_3_5', 'under_4_5'
     ]
